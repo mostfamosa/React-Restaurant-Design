e21ed41ed15f8411a37046583ab2be2b19df1197
Mostafa Mossa <mostfa.g.m@gmail.com>
2023-06-04 02:22:46 +0300

    finished with the header and subheader for the app

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
-
 import './Header.css';
+import {images} from '../../constants/'
+import { SubHeading } from '../../components';
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
+
+function Header() {
+  return (
+    <div className='app__header app__wrapper section__padding' id='home'>
+      <div className='app__wrapper_info'>
+        <SubHeading title="Chase The New Flavor"/>
+        <h1 className='app__header-h1'>The Key To Find Dinning</h1>
+      <p className='p__opensans' style={{margin:'2rem 0'}}>Sit tellus lobortis sed senectus vivamus molestie. Condimentum volutpat morbi facilisis quam scelerisque sapien. Et, penatibus aliquam amet tellus </p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+      </div>
+      <div className='app__wrapper_img'>
+        <img src={images.welcome} alt='header-img'/>
+      </div>
+    </div>
+  );
+}
 
 export default Header;
