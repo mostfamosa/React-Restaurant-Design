e5b3f37977c33c3eecfbdc19f2ee2dfeb0b05b54
Mostafa Mossa <mostfa.g.m@gmail.com>
2023-06-04 01:59:21 +0300

    finished responsive navbar for the app

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
-
+import React, { useState } from 'react';
 import './Navbar.css';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineRestaurantMenu  } from 'react-icons/md';
+import images from '../../constants/images';
 
-const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+function Navbar() {
+
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+
+
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.gericht} alt='app logo' />
+      </div>
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><a href='#home'>Home</a></li>
+        <li className='p__opensans'><a href='#about'>About</a></li>
+        <li className='p__opensans'><a href='#menu'>Menu</a></li>
+        <li className='p__opensans'><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className='app__navbar-login'>
+        <a href='#login' className='p__opensans'>Log In/Register</a>
+        <div />
+        <a href='/' className='p__opensans'>Book Table</a>
+      </div>
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color='fff' fontSize={27} onClick={() => setToggleMenu(true)} />
+        {toggleMenu && (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdOutlineRestaurantMenu fontSize={27} onClick={() => setToggleMenu(false)} className='overlay__close' />
+            <ul className='app__navbar-smallscreen_links'>
+              <li className='p__opensans'><a href='#home'>Home</a></li>
+              <li className='p__opensans'><a href='#about'>About</a></li>
+              <li className='p__opensans'><a href='#menu'>Menu</a></li>
+              <li className='p__opensans'><a href='#contact'>Contact</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
+
