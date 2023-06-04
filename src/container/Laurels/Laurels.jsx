dfc4a0e1bf7cb0f5d1897ff3080c40c688e633d7
Mostafa Mossa <mostfa.g.m@gmail.com>
2023-06-04 16:19:20 +0300

    adding small section of reward (laurels)

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
-
+import { images, data } from '../../constants/'
+import { SubHeading, MenuItem } from '../../components';
 import './Laurels.css';
 
-const Laurels = () => (
-  <div>
-    Laurels
+const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
+  <div className='app__laurels_awards-card'>
+    <img src={imgUrl} alt='award' />
+    <div className='app__laurels_awards-card_content'>
+      <p className='p__cormorant' style={{ color: '#DCCA87' }}>{title}</p>
+      <p className='p__cormorant'>{subtitle}</p>
+    </div>
   </div>
 );
+
+
+function Laurels() {
+  return (
+    <div className='app__bg app__wrapper section__padding' id="awards">
+      <div className='app__wrapper_info'>
+        <SubHeading title="Awards & recognition" />
+        <h1 className='headtext__cormorant'>Our Laurels</h1>
+
+        <div className='app__laurels_awards'>
+          {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+        </div>
+
+
+      </div>
+      <div className='app__wrapper_img'>
+        <img src={images.laurels} alt='laurels' />
+      </div>
+    </div>
+  );
+}
 
 export default Laurels;
