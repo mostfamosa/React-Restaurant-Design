6708f089040202c62fbe03f7ef892cb4e8c3fcf1
Mostafa Mossa <mostfa.g.m@gmail.com>
2023-06-04 17:29:47 +0300

    adding about us section

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { images } from '../../constants/'
+import { SubHeading } from '../../components';
 
-const FindUs = () => (
-  <div>
-    FindUs
-  </div>
-);
+function FindUs() {
+  return (
+    <div id='contact' className='app__bg app__wrapper section__padding'>
+      <div className='app__wrapper_info'>
+        <SubHeading title="Contact"/>
+        <h1 className='headtext__cormorant' style={{marginBottom:'3rem'}}>Find Us</h1>
+        <div className='app__wrapper-content'>
+          <p className='p__opensans'>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+          <p className='p__cormorant' style={{color:'#DDCA87',margin:'2rem 0'}}>Opening Hours</p>
+          <p className='p__opensans'>Mon - Fri: 10:00 am - 02:00 am</p>
+          <p className='p__opensans'>Sat - Sun: 10:00 am - 03:00 am</p>
+        </div>
+        <button className='custom__button' style={{marginTop:'2rem'}}>Visit Us</button>
+      </div>
+
+      <div className='app__wrapper_img'>
+        <img src={images.findus} alt='findus-img'/>
+      </div>
+    </div>
+  );
+}
 
 export default FindUs;
